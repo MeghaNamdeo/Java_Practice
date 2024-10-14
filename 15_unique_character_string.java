5a4772333b592f25c8bd4899329de26fd5dc4361
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-10-14 11:51:30 +0530

    java

--- 15_unique_character_string.java
+++ 15_unique_character_string.java
@@ -32,3 +32,27 @@ public class Main {
         sc.close();
     }
 }
+*******************************--------------------------**************************
+    
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	str=str.toLowerCase();
+	
+	String ans="";
+	
+	Set<Character>seen=new HashSet<>();
+	
+	for(int i=0;i<str.length();i++)
+	{
+	    char ch = str.charAt(i);
+	    if(seen.add(ch))
+	    ans=ans+ch;
+	}
+	System.out.println(ans);
+	}
+}
