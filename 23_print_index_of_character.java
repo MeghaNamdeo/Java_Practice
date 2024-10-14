b8885211b4be3a62543cca6de4360dd05cfa60c7
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-10-14 15:53:06 +0530

    java

---
+++ 23_print_index_of_character.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+Example :
+input : Xplore
+charcter :r
+output :4
+
+
+*******************************************************************************/
+import java.util.*;
+public class Main{
+    public static void main (String args[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        String str= sc.nextLine();
+        
+        char ch=sc.next().charAt(0);
+        
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)==ch)
+            {
+                System.out.println(i);
+                break;
+            }
+            
+        }
+        
+    }
+}
