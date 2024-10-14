a231a35642c3c9eb022b822ebc09fcaaa6939257
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-10-14 21:44:35 +0530

    java

--- 24_Print last character of each word in a string.java
+++ 24_Print last character of each word in a string.java
@@ -1,1 +1,28 @@
-
+/***************************************************************************
+ Write a program to print the last charcter of every word in a String
+ condition : ignoew all the digits and whitespace
+ input  :Hey3 Java Learners
+ output : as
+ 
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc= new Scanner(System.in);
+	  String str=sc.nextLine();
+	  str=str+' ';
+	  for(int i=0;i<str.length();i++)
+	  {
+	      if(str.charAt(i)==' ')
+	      {
+	         if(i>0&& !Character.isDigit(str.charAt(i-1)))
+	         {
+	             System.out.print(str.charAt(i-1));
+	         }
+	      }
+	  }
+	  
+	}
+	
+}
