eac17c143d718d9ea0764e5b2982aefa20b839d2
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-10-14 12:28:15 +0530

    java

--- 19_ Find First Palindromic String in the Array.java
+++ 19_ Find First Palindromic String in the Array.java
@@ -1,4 +1,13 @@
-class Solution {
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+    
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is returned.
+    
+    class Solution {
     boolean isPalindrome(String str)
     {
         int start=0;
