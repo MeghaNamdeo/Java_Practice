6ff967d075e819a37d82f3639e8bafe33c9cd7d2
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-10-15 18:36:21 +0530

    java

--- 18_reverse_string.java
+++ 18_reverse_string.java
@@ -1,3 +1,47 @@
+/**************************************************************************coeditor and press "Run" button to execute it.
+Reverse string
+
+input 
+Enter the String: Hello World from TCS
+output
+The String Output: olleH dlroW morf SCT
+
+
+*******************************************************************************/
+
+import java.util.*;
+public class Main {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the String: ");
+		
+		String str=sc.nextLine();
+		//things to remember 
+	    char arr[]= str.toCharArray();
+		
+	
+		int start=0;
+		int end=arr.length-1;
+		
+		
+		while(start<=end)
+		{  
+		    char temp=arr[start];
+		    arr[start]=arr[end];
+		    arr[end]=temp;
+		    start++;
+		    end--;
+		    
+		    
+		}
+		System.out.print("Reverse String : ");
+		for(int i=0;i<arr.length;i++)
+		System.out.print(arr[i]);
+	}
+}
+
+
 /******************************************************************************
 
 Write a java program to reverse a given String
